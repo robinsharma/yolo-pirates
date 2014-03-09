da38653054a501297bb7ac3d963d4ab5396107c1
jmuia <joseph.b.muia@gmail.com>
2014-03-09 03:38:07 -0400

    Outline for yolo pirate receive kik message page

--- web/js/page-message.js
+++ web/js/page-message.js
@@ -1,3 +1,24 @@
 App.populator ('message', function ($page, data) {
 	console.log(data);
+	var $pirateBox = $page.querySelector('TO-DO'),
+		$homeButton = $page.querySelector('TO-DO'),
+		$fwdButton = $page.querySelector('TO-DO');
+
+	$pirateBox.value = data;
+
+	$homeButton.addEventListener('click', function() {
+		App.back();
+	});
+
+	$fwdButton.addEventListener('click', function() {
+		if (kik.send) {
+			kik.send({
+				title: 'Yolo Pirate Message',
+				text: data,
+				data: { msg: data }
+			});
+		}
+	});
+
+
 });
