1a3e66b80f64ea5baf02cb33d9fe9ab682db2916
Robin Sharma <sharms115@gmail.com>
2014-03-09 01:53:18 -0500

    Logo yolo

--- web/js/page-home.js
+++ web/js/page-home.js
@@ -1,14 +1,14 @@
 App.populator( 'home', function ($page) {
-	var $textBox    = $page.querySelector('.message-box'),
-		$sendButton = $page.querySelector('.send-button');
+	// var $textBox    = $page.querySelector('.message-box'),
+	// 	$sendButton = $page.querySelector('.send-button');
 
-	$sendButton.addEventListener('click', function(){
-		API.getYoloPirates($textBox.value, function (data) {
-			if (data.err) {
-				//TODO
-			} else {
-				App.load('message', data);
-			}
-		});
-	});
+	// $sendButton.addEventListener('click', function(){
+	// 	API.getYoloPirates($textBox.value, function (data) {
+	// 		if (data.err) {
+	// 			//TODO
+	// 		} else {
+	// 			App.load('message', data);
+	// 		}
+	// 	});
+	// });
 });
