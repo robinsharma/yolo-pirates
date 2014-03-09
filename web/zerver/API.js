9722d1870a0015293c8791789e8f284b1694ec06
jmuia <joseph.b.muia@gmail.com>
2014-03-09 00:04:27 -0500

    Call ArrPI and translate that yolo pirate talk

--- web/zerver/API.js
+++ web/zerver/API.js
@@ -1,2 +1,20 @@
 var https = require('https');
 
+var BASE_URL = 'http://isithackday.com/arrpi.php';
+
+exports.getYoloPirates = function (msg, callback) {
+	http.get(encodeURI(base+'?text='+msg), function (res) {
+		var pageData = "";
+		res.on('data', function (chunk) {
+			pageData += chunk;
+		});
+
+		res.on('end', function() {
+			callback(pageData);
+		});
+
+	}).on('error', function (e) {
+		console.log("Got error: " + e.message);
+	});
+}
+
