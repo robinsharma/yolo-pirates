72149d142967096309ff3d9bc3ec3f310acc728f
Kevin McGaire <mcga7560@mylaurier.ca>
2014-03-16 22:17:28 -0400

    Dat refactor doe

--- web/js/page-home.js
+++ web/js/page-home.js
@@ -1,94 +1,104 @@
-App.populator( 'home', function ($page) {
-	var previewText,
-		$textBox           = $page.querySelector('.message-box'),
-		$sendButton        = $page.querySelector('.send-button'),
-		$sendButtonText    = $sendButton.querySelector('.button-text'),
-		$previewButton     = $page.querySelector('.preview-button'),
-		$previewButtonText = $previewButton.querySelector('.button-text'),
-		$previewBox        = $page.querySelector('.preview-text'),
-		$previewSpinner    = $previewButton.querySelector('.spinner'),
-		$sendSpinner       = $sendButton.querySelector('.spinner'),
-		previewed          = false;
+App.populator('home', function ($page) {
+  var previewText,
+    $textBox           = $page.querySelector('.message-box'),
+    $sendButton        = $page.querySelector('.send-button'),
+    $sendButtonText    = $sendButton.querySelector('.button-text'),
+    $previewButton     = $page.querySelector('.preview-button'),
+    $previewButtonText = $previewButton.querySelector('.button-text'),
+    $previewBox        = $page.querySelector('.preview-text'),
+    $previewSpinner    = $previewButton.querySelector('.spinner'),
+    $sendSpinner       = $sendButton.querySelector('.spinner'),
+    previewed          = false;
 
-	$previewButton.addEventListener('click', function() {
-		if ($textBox.value) {
-			if (previewed) {
-				if (previewText !== $textBox.value) {
-					$previewButtonText.innerText = '';
-					$previewSpinner.classList.add('enabled');
-					piratizeMessage($textBox.value, function (pmsg) {
-						$previewButtonText.innerText = 'Preview';
-						$previewSpinner.classList.remove('enabled');
-						if (pmsg) {
-							$previewBox.innerText = previewText = pmsg;
-						}
-					});
-				}
-			} else {
-				previewed = true;
-				$previewButtonText.innerText = '';
-				$previewSpinner.classList.add('enabled');
-				piratizeMessage($textBox.value, function (pmsg) {
-					$previewButtonText.innerText = 'Preview';
-					$previewSpinner.classList.remove('enabled');
-					if (pmsg) {
-						$previewBox.innerText = previewText = pmsg;
-					}
-				});
-			}
-		}
-	});
 
-	$sendButton.addEventListener('click', function(){
-		if (kik.send && $textBox.value) {
-			if (previewed) {
-				if (previewText !== $textBox.value) {
-					$sendButtonText.innerText = '';
-					$sendSpinner.classList.add('enabled');
-					piratizeMessageAndSend($textBox.value, function () {
-						$sendButtonText.innerText = 'Send';
-						$sendSpinner.classList.remove('enabled');
-					});
-				} else {
-					kik.send({
-            title : 'message ye scurvy dog! Tap to open thar!',
-            pic   : '/img/yolo_pirate.png',
-            data  : { msg: previewText }  });
-				}
-			} else {
-				$sendButtonText.innerText = '';
-				$sendSpinner.classList.add('enabled');
-				piratizeMessageAndSend($textBox.value, function () {
-					$sendButtonText.innerText = 'Send';
-					$sendSpinner.classList.remove('enabled');
-				});
-			}
-		}
-	});
+  $previewButton.addEventListener('click', function () {
+      if (hasTextToPreview()) {
+        turnOnSpinner($previewSpinner);
+        piratizeMessage($textBox.value, function (pmsg) {
+          turnOffSpinner($previewSpinner)
+          if (pmsg) {
+            $previewBox.innerText = pmsg;
+            previewText = $textBox.value;
+            previewed = true;
+          }
+        });
+      } else {
+        turnOnSpinner($previewSpinner)
+        setTimeout(function () {
+          turnOffSpinner($previewSpinner)
+        }, 500);
+      }
+    }
+  );
+
+  $sendButton.addEventListener('click', function () {
+    if (hasTextToPreview()) {
+      turnOnSpinner($sendSpinner)
+      sendMessage($previewBox.innerText.value, function () {
+        turnOffSpinner($sendSpinner)
+      });
+    } else if ($textBox.value) {
+      turnOnSpinner($sendSpinner)
+      piratizeMessageAndSend($textBox.value, function () {
+        turnOffSpinner($sendSpinner)
+      });
+    } else {
+    }
+  });
+
+  function hasTextToPreview() {
+    return (($textBox.value) && (!previewed || (previewText !== $textBox.value)));
+  }
+
+  function turnOnSpinner(spinner) {
+    if (spinner === $sendSpinner) {
+      $sendButtonText.innerText = '';
+      $sendSpinner.classList.add('enabled');
+    } else {
+      $previewButtonText.innerText = '';
+      $previewSpinner.classList.add('enabled');
+    }
+  }
+
+  function turnOffSpinner(spinner) {
+    if (spinner === $sendSpinner) {
+      $sendButtonText.innerText = 'Send';
+      $sendSpinner.classList.remove('enabled');
+    } else {
+      $previewButtonText.innerText = 'Preview';
+      $previewSpinner.classList.remove('enabled');
+    }
+  }
+
+  function piratizeMessage(msg, callback) {
+    ArrPI.getYoloPirates(msg, function (data) {
+      if (data.err) {
+        //TODO
+        console.log('Error: ' + data.err);
+        callback();
+      } else {
+        console.log('Pirate msg: ' + data.translation);
+        callback(data.translation);
+      }
+    });
+  }
+
+  function sendMessage(pmsg, callback) {
+    if (kik.send) {
+      kik.send({
+        title: 'message ye scurvy dog! Tap to open thar!',
+        pic  : '/img/yolo_pirate.png',
+        data : { msg: pmsg }
+      });
+      callback();
+    }
+  }
+
+  function piratizeMessageAndSend(msg, callback) {
+    piratizeMessage(msg, function (pmsg) {
+      if (pmsg) sendMessage(pmsg, callback);
+    });
+  }
+
 });
 
-function piratizeMessage(msg, callback) {
-	ArrPI.getYoloPirates(msg, function (data) {
-		if (data.err) {
-			//TODO
-			console.log('Error: ' + data.err);
-			callback();
-		} else {
-			console.log('Pirate msg: ' + data.translation);
-			callback(data.translation);
-		}
-	});
-}
-
-function piratizeMessageAndSend (msg, callback) {
-	piratizeMessage(msg, function (pmsg) {
-		if (pmsg && kik.send) {
-			kik.send({
-				title: 'message ye scurvy dog! Tap to open thar!',
-				pic: '/img/yolo_pirate.png',
-				data: { msg: pmsg }
-			});
-		}
-		callback();
-	});
-}
