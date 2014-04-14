deb9931044faf61d69cb6f0c84997957509951fd
Joseph Muia <joseph.b.muia@gmail.com>
2014-04-14 15:57:27 -0400

    added banner stuff

--- web/js/page-home.js
+++ web/js/page-home.js
@@ -1,116 +1,126 @@
 App.populator('home', function ($page) {
-  var previewText,
-    $textBox           = $page.querySelector('.message-box'),
-    $sendButton        = $page.querySelector('.send-button'),
-    $sendButtonText    = $sendButton.querySelector('.button-text'),
-    $previewButton     = $page.querySelector('.preview-button'),
-    $previewButtonText = $previewButton.querySelector('.button-text'),
-    $previewBox        = $page.querySelector('.preview-text'),
-    $previewSpinner    = $previewButton.querySelector('.spinner'),
-    $sendSpinner       = $sendButton.querySelector('.spinner'),
-    previewed          = false;
+	var previewText,
+	$textBox           = $page.querySelector('.message-box'),
+	$sendButton        = $page.querySelector('.send-button'),
+	$sendButtonText    = $sendButton.querySelector('.button-text'),
+	$previewButton     = $page.querySelector('.preview-button'),
+	$previewButtonText = $previewButton.querySelector('.button-text'),
+	$previewBox        = $page.querySelector('.preview-text'),
+	$previewSpinner    = $previewButton.querySelector('.spinner'),
+	$sendSpinner       = $sendButton.querySelector('.spinner'),
+	previewed          = false;
 
 
-  $previewButton.addEventListener('click', function () {
-      if (apiCallNeeded()) {
-        turnOnSpinner($previewSpinner);
-        piratizeMessage($textBox.value, function (pmsg) {
-          turnOffSpinner($previewSpinner)
-        });
-      } else {
-        turnOnSpinner($previewSpinner)
-        setTimeout(function () {
-          turnOffSpinner($previewSpinner)
-        }, 500);
-      }
-    }
-  );
+	$previewButton.addEventListener('click', function () {
+		if (apiCallNeeded()) {
+			turnOnSpinner($previewSpinner);
+			piratizeMessage($textBox.value, function (pmsg) {
+				turnOffSpinner($previewSpinner)
+			});
+		} else {
+			turnOnSpinner($previewSpinner)
+			setTimeout(function () {
+				turnOffSpinner($previewSpinner)
+			}, 500);
+		}
+	}
+	);
 
-  $sendButton.addEventListener('click', function () {
-    if (apiCallNeeded()) {
-      turnOnSpinner($sendSpinner)
-      piratizeMessageAndSend($textBox.value, function () {
-        turnOffSpinner($sendSpinner)
-      });
-    } else if ($textBox.value) {
-      turnOnSpinner($sendSpinner)
-      sendMessage($previewBox.innerText, function () {
-        turnOffSpinner($sendSpinner)
-      });
-    } else {
-    }
-  });
+	$sendButton.addEventListener('click', function () {
+		if (apiCallNeeded()) {
+			turnOnSpinner($sendSpinner)
+			piratizeMessageAndSend($textBox.value, function () {
+				turnOffSpinner($sendSpinner)
+			});
+		} else if ($textBox.value) {
+			turnOnSpinner($sendSpinner)
+			sendMessage($previewBox.innerText, function () {
+				turnOffSpinner($sendSpinner)
+			});
+		} else {
+		}
+	});
 
-  function apiCallNeeded() {
-    return (($textBox.value) && (!previewed || (previewed && (previewText !== $textBox.value))));
-  }
+	RequestAd_({
+	    // Ad Request Variables
+	    s:'5d529f83cf677b47d12dcb4e1459d7fd',
+	    m:'live',
+	    backfillhtml:'',
+	    prependclickcontent:'',
+	    requesturl:'http://kik-madserve.herokuapp.com/md.request.php',
+	    trackingpixelurl:''
+	}, page.querySelector("#banner-container"));
 
-  function turnOnSpinner(spinner) {
-    if (spinner === $sendSpinner) {
-      $sendButtonText.innerText = '';
-      $sendSpinner.classList.add('enabled');
-    } else {
-      $previewButtonText.innerText = '';
-      $previewSpinner.classList.add('enabled');
-    }
-  }
+	function apiCallNeeded() {
+		return (($textBox.value) && (!previewed || (previewed && (previewText !== $textBox.value))));
+	}
 
-  function turnOffSpinner(spinner) {
-    if (spinner === $sendSpinner) {
-      $sendButtonText.innerText = 'Send';
-      $sendSpinner.classList.remove('enabled');
-    } else {
-      $previewButtonText.innerText = 'Preview';
-      $previewSpinner.classList.remove('enabled');
-    }
-  }
+	function turnOnSpinner(spinner) {
+		if (spinner === $sendSpinner) {
+			$sendButtonText.innerText = '';
+			$sendSpinner.classList.add('enabled');
+		} else {
+			$previewButtonText.innerText = '';
+			$previewSpinner.classList.add('enabled');
+		}
+	}
 
-  function piratizeMessage(msg, callback) {
-    ArrPI.getYoloPirates(msg, function (data) {
-      if (!data || data.err) {
-        App.dialog ({
-          title        : 'Connection Error',
-          text         : 'Please check your connection before trying again.' ,
-          okButton     : 'Try Again' , 
-          cancelButton : 'Cancel'
-        }, function (retry) {
-          if (retry) {
-            piratizeMessage(msg, callback);
-          } else {
-            callback();
-          }
-        });
-      } else {
-        callback(data.translation);
-        updatePreview(data.translation);
-      }
-    });
-  }
+	function turnOffSpinner(spinner) {
+		if (spinner === $sendSpinner) {
+			$sendButtonText.innerText = 'Send';
+			$sendSpinner.classList.remove('enabled');
+		} else {
+			$previewButtonText.innerText = 'Preview';
+			$previewSpinner.classList.remove('enabled');
+		}
+	}
 
-  function sendMessage(pmsg, callback) {
-    if (kik.send) {
-      kik.send({
-        title: 'message ye scurvy dog! Tap to open thar!',
-        pic  : '/img/yolo_pirate.png',
-        data : { msg: pmsg }
-      });
-      callback();
-    }
-  }
+	function piratizeMessage(msg, callback) {
+		ArrPI.getYoloPirates(msg, function (data) {
+			if (!data || data.err) {
+				App.dialog ({
+					title        : 'Connection Error',
+					text         : 'Please check your connection before trying again.' ,
+					okButton     : 'Try Again' , 
+					cancelButton : 'Cancel'
+				}, function (retry) {
+					if (retry) {
+						piratizeMessage(msg, callback);
+					} else {
+						callback();
+					}
+				});
+			} else {
+				callback(data.translation);
+				updatePreview(data.translation);
+			}
+		});
+	}
 
-  function piratizeMessageAndSend(msg, callback) {
-    piratizeMessage(msg, function (pmsg) {
-      if (pmsg) sendMessage(pmsg, callback);
-    });
-  }
+	function sendMessage(pmsg, callback) {
+		if (kik.send) {
+			kik.send({
+				title: 'message ye scurvy dog! Tap to open thar!',
+				pic  : '/img/yolo_pirate.png',
+				data : { msg: pmsg }
+			});
+			callback();
+		}
+	}
 
-  function updatePreview(msg) {
-    if (msg) {
-      $previewBox.innerText = msg;
-      previewText = $textBox.value;
-      previewed = true;
-    }
-  }
+	function piratizeMessageAndSend(msg, callback) {
+		piratizeMessage(msg, function (pmsg) {
+			if (pmsg) sendMessage(pmsg, callback);
+		});
+	}
+
+	function updatePreview(msg) {
+		if (msg) {
+			$previewBox.innerText = msg;
+			previewText = $textBox.value;
+			previewed = true;
+		}
+	}
 
 });
 
